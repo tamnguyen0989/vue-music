a43ceec3082f5d11f51346e84f956c1d8b3f8350
tam nguyen <tamnguyen0989@gmail.com>
2022-06-21 11:25:13 +0700

    add Validation

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    authModalShow: false
   },
   getters: {
+    // authModalShow: (state) => state.authModalShow,
   },
   mutations: {
+    toogleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow
+    }
   },
   actions: {
   },
